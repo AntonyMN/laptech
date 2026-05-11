976ca928c8e902c60ea1c4f13e692dafb023931f
Antony Muriuki <antonymuriuki7@gmail.com>
2026-05-11 09:49:14 +0300

    fix: add missing relationships to Category and ServiceCategory models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['name', 'slug', 'description', 'image'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
 
--- app/Models/ServiceCategory.php
+++ app/Models/ServiceCategory.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ServiceCategory extends Model
 {
     protected $fillable = ['name', 'slug'];
+
+    public function services()
+    {
+        return $this->hasMany(Service::class);
+    }
 }
